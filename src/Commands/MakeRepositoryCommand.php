57ecaec67f8c8595156ec68bea3c15985da829f2
masgeek <barsamms@gmail.com>
2026-05-11 11:56:55 +0300

    :rotating_light: Add `make:repo` artisan command
    Introduce a new command to generate repository classes with optional model integration.
    
    **Migration steps:**
    - Clear the configuration cache using `php artisan config:clear`.
    
    **Impact assessment:**
    - Enhances the toolkit by adding repository creation functionality, which streamlines repository pattern implementation but may require developer onboarding or documentation updates.

---
+++ src/Commands/MakeRepositoryCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Masgeek\ArtisanToolkit\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class MakeRepositoryCommand extends Command
+{
+    protected $signature = 'make:repo {name} {--model=}';
+
+    protected $description = 'Create a new repository class';
+
+    public function handle(): bool
+    {
+        $name = $this->argument('name');
+        $model = $this->option('model');
+        $repositoryPath = app_path("Repositories/{$name}.php");
+
+        if (file_exists($repositoryPath)) {
+            $this->error("Repository '{$name}' already exists!");
+
+            return false;
+        }
+
+        (new Filesystem)->ensureDirectoryExists(app_path('Repositories'));
+
+        $modelClass = $model ? "\\App\\Models\\{$model}" : '';
+        $modelImport = $model ? "use {$modelClass};" : '';
+        //        $modelVariable = $model ? lcfirst(class_basename($model)) : 'model';
+        $modelVariable = 'model';
+        $modelType = $model ?: 'Model';
+
+        $stub = <<<PHP
+<?php
+
+namespace App\Repositories;
+
+$modelImport
+
+
+/**
+ * @extends \App\Repositories\BaseRepo<$modelType>
+ */
+class {$name} extends \App\Repositories\BaseRepository
+{
+    protected {$modelType} \${$modelVariable};
+
+    protected function model(): string
+    {
+        return {$modelType}::class;
+    }
+}
+PHP;
+
+        file_put_contents($repositoryPath, $stub);
+
+        $this->info("Repository '{$name}' created successfully.");
+
+        return true;
+    }
+}
